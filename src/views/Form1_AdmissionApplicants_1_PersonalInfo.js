0639e0ba95c44545a9bcf6d483565c69907b7c76
thehansguy <thehansguy@gmail.com>
2021-11-07 19:57:59 +0000

    restructuring form ui

--- src/views/Form1_AdmissionApplicants_1_PersonalInfo.js
+++ src/views/Form1_AdmissionApplicants_1_PersonalInfo.js
@@ -29,50 +29,49 @@ function Form1AdmissionApplicants_1_PersonalInfo() {
       </Row>
       <Row className="mb-3">
         <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>Application Payment Reference</Form.Label>
-          <Form.Control placeholder="Application Payment Reference" />
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control placeholder="Last Name" />
         </Form.Group>
 
         <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>Application Payment Reference</Form.Label>
-          <Form.Control placeholder="Application Payment Reference" />
+          <Form.Label>Gender</Form.Label>
+          <Form.Control placeholder="Gender" />
         </Form.Group>
       </Row>
-
       <Row className="mb-3">
         <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>Application Payment Reference</Form.Label>
+          <Form.Label>Date of birth</Form.Label>
+          <Form.Control placeholder="Date of birth" />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Label>Country</Form.Label>
+          <Form.Control placeholder="Country" />
+        </Form.Group>
+      </Row>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>Email</Form.Label>
           <Form.Control placeholder="Application Payment Reference" />
         </Form.Group>
 
         <Form.Group as={Col} controlId="formGridPassword">
-          <Form.Label>Application Reference</Form.Label>
-          <Form.Control placeholder="Application Reference" />
-        </Form.Group>
-      </Row>
-
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>Application Payment Reference</Form.Label>
-          <Form.Control placeholder="Application Payment Reference" />
-        </Form.Group>
-
-        <Form.Group as={Col} controlId="formGridPassword">
-          <Form.Label>Password</Form.Label>
+          <Form.Label>Region</Form.Label>
           <Form.Control placeholder="Password" />
         </Form.Group>
       </Row>
+      <hr />
+      <strong>Permanent Address</strong>
 
+      <br />
       <Form.Group className="mb-3" controlId="formGridAddress1">
         <Form.Label>Address</Form.Label>
         <Form.Control placeholder="1234 Main St" />
       </Form.Group>
-
       <Form.Group className="mb-3" controlId="formGridAddress2">
         <Form.Label>Address 2</Form.Label>
         <Form.Control placeholder="Apartment, studio, or floor" />
       </Form.Group>
-
       <Row className="mb-3">
         <Form.Group as={Col} controlId="formGridCity">
           <Form.Label>City</Form.Label>
@@ -92,11 +91,109 @@ function Form1AdmissionApplicants_1_PersonalInfo() {
           <Form.Control />
         </Form.Group>
       </Row>
-
       <Form.Group className="mb-3" id="formGridCheckbox">
         {/* <Form.Check type="checkbox" label="Check me out" /> */}
       </Form.Group>
+      <hr />
+      <strong>Current Address</strong>
+      <br />
+      <Form.Group className="mb-3" controlId="formGridAddress1">
+        <Form.Label>Address</Form.Label>
+        <Form.Control placeholder="1234 Main St" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formGridAddress2">
+        <Form.Label>Address 2</Form.Label>
+        <Form.Control placeholder="Apartment, studio, or floor" />
+      </Form.Group>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>City</Form.Label>
+          <Form.Control />
+        </Form.Group>
 
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>State</Form.Label>
+          <Form.Control as="select" defaultValue="Choose...">
+            <option>Choose...</option>
+            <option>...</option>
+          </Form.Control>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridZip">
+          <Form.Label>Zip</Form.Label>
+          <Form.Control />
+        </Form.Group>
+      </Row>
+      <Form.Group className="mb-3" id="formGridCheckbox">
+        {/* <Form.Check type="checkbox" label="Check me out" /> */}
+      </Form.Group>
+      <hr />
+      <strong>Home Phone</strong>
+      <br />
+      <Form.Group className="mb-3" controlId="formGridAddress1">
+        <Form.Label>Address</Form.Label>
+        <Form.Control placeholder="1234 Main St" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formGridAddress2">
+        <Form.Label>Address 2</Form.Label>
+        <Form.Control placeholder="Apartment, studio, or floor" />
+      </Form.Group>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>City</Form.Label>
+          <Form.Control />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>State</Form.Label>
+          <Form.Control as="select" defaultValue="Choose...">
+            <option>Choose...</option>
+            <option>...</option>
+          </Form.Control>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridZip">
+          <Form.Label>Zip</Form.Label>
+          <Form.Control />
+        </Form.Group>
+      </Row>
+      <Form.Group className="mb-3" id="formGridCheckbox">
+        {/* <Form.Check type="checkbox" label="Check me out" /> */}
+      </Form.Group>
+      <hr />
+      <strong>Mobile</strong>
+      <br />
+      <Form.Group className="mb-3" controlId="formGridAddress1">
+        <Form.Label>Address</Form.Label>
+        <Form.Control placeholder="1234 Main St" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formGridAddress2">
+        <Form.Label>Address 2</Form.Label>
+        <Form.Control placeholder="Apartment, studio, or floor" />
+      </Form.Group>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>City</Form.Label>
+          <Form.Control />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridState">
+          <Form.Label>State</Form.Label>
+          <Form.Control as="select" defaultValue="Choose...">
+            <option>Choose...</option>
+            <option>...</option>
+          </Form.Control>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridZip">
+          <Form.Label>Zip</Form.Label>
+          <Form.Control />
+        </Form.Group>
+      </Row>
+      <Form.Group className="mb-3" id="formGridCheckbox">
+        {/* <Form.Check type="checkbox" label="Check me out" /> */}
+      </Form.Group>
+      <br />
       <Button variant="success" type="submit">
         Admit
       </Button>
--- src/views/Form1_AdmissionApplicants_4_AttachedDocuments.js
+++ src/views/Form1_AdmissionApplicants_4_AttachedDocuments.js
@@ -4,60 +4,28 @@ import { Button, Row, Col, Form } from "react-bootstrap";
 
 function Form1AdmissionApplicants_4_AttachedDocuments() {
   return (
-    <Form>
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-
-        <Form.Group as={Col} controlId="formGridPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-      </Row>
-
-      <Form.Group className="mb-3" controlId="formGridAddress1">
-        <Form.Label>Address</Form.Label>
-        <Form.Control placeholder="1234 Main St" />
+    <>
+      <Form.Group controlId="formFile" className="mb-3">
+        <Form.Label>Default file input example</Form.Label>
+        <Form.Control type="file" />
       </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formGridAddress2">
-        <Form.Label>Address 2</Form.Label>
-        <Form.Control placeholder="Apartment, studio, or floor" />
+      <Form.Group controlId="formFileMultiple" className="mb-3">
+        <Form.Label>Multiple files input example</Form.Label>
+        <Form.Control type="file" multiple />
       </Form.Group>
-
-      <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridCity">
-          <Form.Label>City</Form.Label>
-          <Form.Control />
-        </Form.Group>
-
-        <Form.Group as={Col} controlId="formGridState">
-          <Form.Label>State</Form.Label>
-          <Form.Control as="select" defaultValue="Choose...">
-            <option>Choose...</option>
-            <option>...</option>
-          </Form.Control>
-        </Form.Group>
-
-        <Form.Group as={Col} controlId="formGridZip">
-          <Form.Label>Zip</Form.Label>
-          <Form.Control />
-        </Form.Group>
-      </Row>
-
-      <Form.Group className="mb-3" id="formGridCheckbox">
-        {/* <Form.Check type="checkbox" label="Check me out" /> */}
+      <Form.Group controlId="formFileDisabled" className="mb-3">
+        <Form.Label>Disabled file input example</Form.Label>
+        <Form.Control type="file" disabled />
       </Form.Group>
-
-      <Button variant="success" type="submit">
-        Admit
-      </Button>
-      <Button variant="danger" type="submit">
-        Reject
-      </Button>
-    </Form>
+      <Form.Group controlId="formFileSm" className="mb-3">
+        <Form.Label>Small file input example</Form.Label>
+        <Form.Control type="file" size="sm" />
+      </Form.Group>
+      <Form.Group controlId="formFileLg" className="mb-3">
+        <Form.Label>Large file input example</Form.Label>
+        <Form.Control type="file" size="lg" />
+      </Form.Group>
+    </>
   );
 }
 
